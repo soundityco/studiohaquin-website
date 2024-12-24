55e6c338d504f2826051d6d1e7fbd12e4cff0640
SoundLnk <polyphoniamusic.mgmt@gmail.com>
2024-12-24 18:37:49 +0100

    Update V30.1.7

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c84940f37e114d64"; }
+/******/ 		__webpack_require__.h = function() { return "b60263ddbed7b6d3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Intro.js
+++ components/Intro.js
@@ -17,18 +17,6 @@ function Intro() {
             </span>
           </p>
         </div>
-        <div className="intro-container-item">
-          <h3 className="intro-container-title">01. ABOUT</h3>
-          <p className="intro-container-text">
-            <span>Filmmaker, sound designer & compositeur, basé à Angers.</span>
-            <br />
-            <span>
-              De la prise de vue au montage, en passant par la conception sonore et graphique, j’apporte une attention
-              particulière aux détails pour vous offrir des créations qui reflètent votre identité, afin de raconter
-              votre histoire de façon authentique et impactante.
-            </span>
-          </p>
-        </div>
       </div>
     </section>
   );
