cdfdfb1156e0ca0d9d1c2cad4aecce228cb36dca
SoundLnk <polyphoniamusic.mgmt@gmail.com>
2025-01-08 12:47:24 +0100

    Update v30.8.1

--- components/Hero.js
+++ components/Hero.js
@@ -9,7 +9,7 @@ export function Hero() {
     <section className="hero" id="hero">
       <Header/>
       <VideoPlayer className="container"
-        videoSrc="https://res.cloudinary.com/dqrkeb9bz/video/upload/q_auto:best/v1736329746/l9hvvgac0iknidcut5rc.mp4"
+        videoSrc="https://res.cloudinary.com/dqrkeb9bz/video/upload/q_auto:best/v1736336766/SHOWREEL_2024_STUDIOHAQUIN_FHD4_h0klpd.mp4"
         //videoSrc="https://live.staticflickr.com/video/54204971545/1d3b0d6e19/1080p.mp4?s=eyJpIjo1NDIwNDk3MTU0NSwiZSI6MTczNDIxODkxNCwicyI6ImY1MTU5YWNhN2UyOTQ0MmEyMjMzY2RhMTA4NGVhNzY0MmY2NGE4Y2EiLCJ2IjoxfQ"
         posterSrc="/showreel-thumbnail.webp"
       />
